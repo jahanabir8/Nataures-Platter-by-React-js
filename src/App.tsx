326da73b70c736493801f30da7c2c68a4c2b5e43
jahanabir8 <onupomabir@gmail.com>
2026-09-06 21:56:12 +0300

    basic files installtion

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NavBar from "./Component/Nav/NavBar";
+const App = () => {
+  return (
+    <div className="container m-w-[1380px]">
+      <NavBar></NavBar>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Component/Nav/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div>
+            <p>asd</p>
+        </div>
+    );
+};
+
+export default NavBar;
